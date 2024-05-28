702e42b0990862a34b249d8398d032f8b8887b1a
Justin Steffen <jcsteffen03@gmail.com>
2024-05-28 17:40:40 -0500

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
+
+const yourName = 'Justin Steffen'; 
+newHeader.textContent = `${yourName} is the champion`;
